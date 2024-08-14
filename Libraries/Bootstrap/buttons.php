e061fd96ed93c78d7e7e5a645ac26e52ff907681
Jose Alexis Correa <88249013+codehiggs@users.noreply.github.com>
2024-08-13 19:47:03 -0500

    Update buttons.php

--- Libraries/Bootstrap/buttons.php
+++ Libraries/Bootstrap/buttons.php
@@ -1,1 +1,12 @@
+<?php
 
+    $btnview= $bootstrap->get_Link("btn-view", array(
+        "size" => "sm",
+        "icon" => ICON_VIEW,
+        "title" => lang("App.View"),
+        "href" => "/sie/progress/view/" . $enrollment['enrollment'],
+        "class" => "btn-outline-primary",
+    ));
+
+
+?>
